81d8ff9de6f3966bb600d6eaa60e632920a355c1
Me <fox.derrick@gmail.com>
2020-01-05 18:09:18 -0500

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	animations: [
+		trigger('divState', [
+			state('normal', style({
+				'background-color': 'red',
+				transform: 'translateX(0)'
+			})),
+			state('highlighted', style({
+				'background-color': 'blue',
+				transform: 'translateX(1000px)'
+			})),
+			transition('normal <=> highlighted', animate(1300))
+		]),
+		trigger('eggState', [
+			state('unhatched', style({
+				transform: 'translateX(0)'
+			})),
+			state('hatched', style({
+				transform: 'translateX(1000px)'
+			})),
+			transition('unhatched <=> hatched', animate(300))
+		])
+	]
 })
 export class AppComponent {
-  title = 'homepage';
+	state = 'normal'
+	otherState = 'normal'
+	eggState = 'unhatched'
+	title = 'homepage';
+
+	onAnimate() {
+		this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+	}
+
+	onEgg() {
+		console.log('clicked')
+		this.state === 'unhatched' ? this.state = 'hatched' : this.state = 'unhatched';
+	}
 }
