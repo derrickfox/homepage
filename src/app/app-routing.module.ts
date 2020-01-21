eeab2e1ab4b8c31a80288121259bff2aef5c4691
Me <fox.derrick@gmail.com>
2020-01-20 22:00:45 -0500

    the one before routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtComponent } from './art/art.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '#', pathMatch: 'full' },
+  { path: 'art', component: ArtComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListButtonComponent } from './list-button/list-button.component';
+import { ArtComponent } from './art/art.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListButtonComponent
+    ListButtonComponent,
+    ArtComponent
   ],
   imports: [
     BrowserModule,
