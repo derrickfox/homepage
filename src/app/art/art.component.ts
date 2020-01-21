1f24ee0969217715b35e49f43f576864799473f9
Me <fox.derrick@gmail.com>
2020-01-21 14:15:59 -0500

    images centerd

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-art',
-  templateUrl: './art.component.html',
-  styleUrls: ['./art.component.css']
+	selector: 'app-art',
+	templateUrl: './art.component.html',
+	styleUrls: ['./art.component.css']
 })
 export class ArtComponent implements OnInit {
+	element: HTMLElement
+	modal = document.getElementById("myModal");
+	img = document.getElementById("myImg");
+	modalImg = document.getElementById("img01");
+	captionText = document.getElementById("caption");
+	span = document.getElementsByClassName("close")[0];
+	images = [
+		{
+			path: "../../assets/images/Untitled_Artwork 1.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 10.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 11.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 12.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 13.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 14.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 15.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 16.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 17.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 18.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 19.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 20.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 21.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 22.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 23.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 5.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 6.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 7.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 8.png"
+		},
+		{
+			path: "../../assets/images/Untitled_Artwork 9.png"
+		}
+	]
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		console.log('art loaded');
+		console.log('this.images', this.images)
+	}
+
+	pictureClicked(src) {
+		console.log('this.modal', this.modal)
+		this.modal.style.display = "block";
+		// this.modalImg.src = src;
+	}
 
 }
